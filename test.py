5a845d70e18c35439e8b69bb026d531db215f2d4
Mohammad Abdullah <hmabdullah78692@gmail.com>
2023-10-29 20:23:30 -0700

    :new

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ path="notebooks/research.ipynb"
 
 dir,file=os.path.split(path)
 
-os.makedirs(dir,exist_ok=True)
+os.makedirs(dir)
 
-with open(path,"w") as f:
-    pass
+"""with open(path,"w") as f:
+    pass"""
