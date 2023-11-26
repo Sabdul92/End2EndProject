8a71db5e4bb512d7733fc9f428d79c0be2ac5be5
Mohammad Abdullah <hmabdullah78692@gmail.com>
2023-11-26 13:38:39 +0530

    artifact updated

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -108,7 +108,7 @@ class DataTransformation:
             train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
-             save_object(
+            save_object(
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
             )
